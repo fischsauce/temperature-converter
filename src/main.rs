c20ed96b1570de3a0f614f8c91fff955fa067f07
Steve Fisher <fishsauce@protonmail.com>
2020-04-24 17:29:17 +0900

    adding basicconversion functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
+use std::io;
+// use std::cmp::Ordering;
+
+
 fn main() {
-    println!("Hello, world!");
+    
+    
+    loop {
+
+        // const celcius: u8 = 1;
+        // const farenheit: u8 = 2;
+
+        // Display instructions:
+        println!("Welcome, please select your INPUT unit:\n");
+        println!("1. for Celcius \n2. for Farenheit");
+
+        // Declare unit type variable and wait for input:
+        let mut unit = String::new();
+        io::stdin().read_line(&mut unit).expect("Failed to read line");
+        // Ensure unit selection is a number between 1..3 (ie 1 or 2):
+        let unit: u8 = match unit.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                continue
+            },
+            
+        };
+        
+        // Get the input temperature:
+        loop {
+            println!("Please enter the temperature to convert:");
+            
+            // Declare temp variable and wait for input:
+            let mut temp = String::new();
+            io::stdin().read_line(&mut temp).expect("Failed to read line");
+            // Ensure temperature input is a number between 1..100:
+            let temp: f64 = match temp.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    continue
+                },
+            };
+
+            // Do the conversion:
+
+            // Celcius to Farenheit:
+            if unit == 1 {
+                let output:f64 = (temp * 1.8) + 32.0;
+                // Print the output:
+                println!(
+                    "\n--------------------------------\n{:.2} celcius = {:.2} farenheit\n--------------------------------\n", temp, output);
+                break
+
+            // Farenheit to Celcius:
+            } else {
+                let output:f64 = (temp - 32.0) / 1.8;
+                // Print the output:
+                println!(
+                    "\n--------------------------------\n{:.2} farenheit = {:.2} celcius\n--------------------------------\n", temp, output);
+                break
+            };
+        };
+    }
 }
